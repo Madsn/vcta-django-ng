38697ebaf5ba2e29a1e4da021aedecee83b60f55
Mikkel Madsen <m@madsn.net>
2017-04-21 12:28:01 +0400

    Django trip model

--- vcta_django/vcta_service/models.py
+++ vcta_django/vcta_service/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Hero(models.Model):
@@ -15,7 +17,9 @@ class Trip(models.Model):
     """
     Represents a Trip.
     """
-    name = models.TextField()
+    user = models.ForeignKey(User)
+    date = models.DateField()
+    distance = models.DecimalField(decimal_places=2, max_digits=5, validators = [MinValueValidator(0.01), MaxValueValidator(300)])
 
     def __str__(self):
-        return self.name
+        return self.user.username + " " + str(self.date) + " " + str(self.distance)
